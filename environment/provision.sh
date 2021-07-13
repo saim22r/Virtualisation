47eac2fc48faa4cc3cf9e4c187b42f6be817acb9
unknown <saimrafiq224@gmail.com>
2021-07-13 16:38:59 +0100

    Update

--- environment/provision.sh
+++ environment/provision.sh
@@ -4,10 +4,11 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install nginx -y
 systemctl status nginx
+
+
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install nodejs -y
 sudo apt-get install python-software-properties -y
 sudo npm install pm2 -g
 sudo echo 'export DB_HOST=mongodb' >> .profile
 
-# This line is written inside the vm
