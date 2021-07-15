c4cb82c9b0076e49a5d500a446ba6ae52f7423bd
unknown <saimrafiq224@gmail.com>
2021-07-15 15:47:52 +0100

    Update

--- environment/db/provision.sh
+++ environment/db/provision.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Update mongodb package
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv D68FA50FEA312927
 echo "deb https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-sudo apt-get update -y
-sudo apt-get upgrade -y
+sudo apt-get update -y # Update package list
+sudo apt-get upgrade -y # Find new versions of packages
+# Install mongodb
 sudo apt-get install -y mongodb-org=3.2.20 mongodb-org-server=3.2.20 mongodb-org-shell=3.2.20 mongodb-org-mongos=3.2.20 mongodb-org-tools=3.2.20
 
-cd /etc
-sudo rm -rf mongod.conf
+cd /etc # Change paths to /etc to change
+sudo rm -rf mongod.conf # Deletes the mongod.conf file
+# Rewrite the mongod.conf file
 sudo echo "
 storage:
   dbPath: /var/lib/mongodb
@@ -23,5 +26,5 @@ net:
 " >> mongod.conf
 
 
-sudo systemctl restart mongod
-sudo systemctl enable mongod
+sudo systemctl restart mongod # Restart mongod
+sudo systemctl enable mongod # Enable and activate mongod
