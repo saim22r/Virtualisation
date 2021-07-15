d6e12ebb67dc066ec0b18a9e3e2db5c9024ccc46
unknown <saimrafiq224@gmail.com>
2021-07-15 15:06:16 +0100

    update

--- environment/db/provision.sh
+++ environment/db/provision.sh
@@ -5,3 +5,23 @@ echo "deb https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse"
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install -y mongodb-org=3.2.20 mongodb-org-server=3.2.20 mongodb-org-shell=3.2.20 mongodb-org-mongos=3.2.20 mongodb-org-tools=3.2.20
+
+cd /etc
+sudo rm -rf mongod.conf
+sudo echo "
+storage:
+  dbPath: /var/lib/mongodb
+  journal:
+    enabled: true
+systemLog:
+  destination: file
+  logAppend: true
+  path: /var/log/mongodb/mongod.log
+net:
+  port: 27017
+  bindIp: 192.168.10.150
+" >> mongod.conf
+
+
+sudo systemctl restart mongod
+sudo systemctl enable mongod
--- environment/provision.sh
+++ environment/provision.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
 
 # Automated installation of nginx when using vagrant up
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install nginx -y
+sudo apt-get update -y # Update package list
+sudo apt-get upgrade -y # Find new versions of packages
+sudo apt-get install nginx -y # Install nginx
+sudo systemctl status nginx # Check nginx status
+
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash - # Update nodeje package
+sudo apt-get install -y nodejs # Install nodejs and npm
+sudo apt-get install python-software-properties -y # Install python dependencies
+sudo npm install -g pm2 # Uses npm to install pm2 package
+
+sudo rm -rf /etc/nginx/sites-available/default
+
+sudo echo "server{
+        listen 80;
+        server_name _;
+        location / {
+        proxy_pass http://192.168.10.100:3000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
+        }
+}" >> /etc/nginx/sites-available/default
+
 sudo systemctl restart nginx
+sudo systemctl enable nginx
 
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs
-# sudo apt-get install -y npm
-sudo apt-get install python-software-properties -y
-sudo npm install -g pm2
+cd environment/app
+node seeds/seed.js
+~/.source
+npm start
 
-cd app/
-# sudo npm install -y
