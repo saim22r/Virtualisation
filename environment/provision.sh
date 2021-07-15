94d4ba08ca8a36faade7fcf608901ea519919735
unknown <saimrafiq224@gmail.com>
2021-07-15 12:25:32 +0100

    Update

--- environment/provision.sh
+++ environment/provision.sh
@@ -4,12 +4,13 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install nginx -y
+sudo systemctl restart nginx
 
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
-sudo apt-get install -y npm
+# sudo apt-get install -y npm
 sudo apt-get install python-software-properties -y
 sudo npm install -g pm2
 
 cd app/
-sudo npm install -y
+# sudo npm install -y
